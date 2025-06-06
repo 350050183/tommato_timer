adecbc39a2d25aee02ea3d56f7fce12122ec60bf
朱淦New <350050183@qq.com>
2025-06-06 23:53:39 +0800

    merge

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tomato_app/utils/l10n/app_localizations.dart';
 
 import '../providers/timer_provider.dart';
 import '../widgets/control_buttons.dart';
